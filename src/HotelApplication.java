28f107881b6a43a2ebaa5827d1dfb397b8b566bf
vicusa01 <tzygler2015@yandex.com>
2023-06-11 14:47:17 +0300

    Initial commit

---
+++ src/HotelApplication.java
@@ -0,0 +1,8 @@
+
+public class HotelApplication {
+
+
+    public static void main(String[] args) {
+         MainMenu.mainMenu();
+    }
+    }
---
+++ src/model/room/IRoom.java
@@ -0,0 +1,11 @@
+package model.room;
+
+import model.room.enums.RoomType;
+
+
+public interface IRoom {
+    public String getRoomNumber();
+    public Double getRoomPrice();
+    public RoomType getRoomType();
+    public boolean isFree();
+}
